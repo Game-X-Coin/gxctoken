0957d43dc29a64929367d2a1fe28cd3247988ab0
blindjh <blindblue@snu.ac.kr>
2019-01-31 00:48:17 +0900

    id inserted

--- function/airdrop.js
+++ function/airdrop.js
@@ -8,41 +8,39 @@ module.exports = async () => {
   try {
     console.log('airdrop Start...');
     const tokenAddress = process.env.GXC_TOKEN_CONTRACT;
-    const from =  process.env.AIRDROP_SOURCE_PUB.toLowerCase();
+    const from = process.env.AIRDROP_SOURCE_PUB.toLowerCase();
+    const airdropStartId = parseInt(process.env.AIRDROP_START_ID);
+    if(!airdropStartId) throw('fromNo is not defined');
+
     const contract = await GXCToken.at(tokenAddress);
+
     let airdropResult = [];
     let airdropList = fs.readFileSync('./data/airdrop_list.csv', 'utf8');
     airdropList = airdropList.split('\n');
     let results = [];
-    let fromAddress = '';
-    let fromFlag = false;
-    airdropList.fr
-    for(let i=1; i<airdropList.length; i++) {
+
+    let startFlag = false;
+    for(let i=0; i<airdropList.length; i++) {
       const elem = airdropList[i];
       let transactionResult = {};
-      const [ address, gxcCount ] = elem.split(',').map(e => e.trim());
-
+      const [ id, address, gxcCount ] = elem.split(',').map(e => e.trim());
+      if(parseInt(id) === airdropStartId) startFlag = true;
+      if(!startFlag) continue;
       
-      if(fromFlag && fromAddress !==address) {
-        console.log(`skip address : ${address}`);
-        continue;
-      }
-      fromFlag = false;
       try {
-        console.log(address, "start...");
+        console.log(id +" : " + address + "start..." );
         const amount = web3.toWei(gxcCount, 'ether'); // 1 GXC (1e18)
         const res = await contract.transfer(address, amount, 
-          {from, gas: 4600000, gasPrice: web3.toWei("4", "gwei")});
+          {from, gas: 4600000, gasPrice: web3.toWei(process.env.AIRDROP_GAS_PRICE, "gwei")});
         console.log(address, "end...");
         transactionResult = [address, gxcCount, 'success', res.tx, '', new Date().toISOString().slice(0,19)];
         console.log(transactionResult);
         airdropResult.push([address, gxcCount, res.tx]);
-        if(i > 10000) break;
       } catch (error) {
         console.error(error);
         transactionResult = [address, gxcCount, 'fail', '', error.message, new Date().toISOString().slice(0,19)];
       }
-      fs.appendFileSync('./data/airdrop_result.csv', transactionResult.join(',') + '\n');
+      fs.appendFileSync('./data/airdrop_result2.csv', transactionResult.join(',') + '\n');
         
     }
     console.log('complete..');
